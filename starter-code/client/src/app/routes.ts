8f7f3ecb3531cfc6e8c1894e137732b93ad2bc3f
Fernando Palma <ferpalma21@github.com>
2017-07-27 16:59:20 +0200

    super almost 2nd iteration

---
+++ starter-code/client/src/app/routes.ts
@@ -0,0 +1,11 @@
+//Crear fichero routes
+//importar rutas
+import { Routes } from '@angular/router';
+//importar componentes
+import { EntryListComponent } from './entry-list/entry-list.component';
+
+//exportar routes y son un array
+export const routes: Routes = [
+  {path: '',component: EntryListComponent},
+  {path: ''}
+]
